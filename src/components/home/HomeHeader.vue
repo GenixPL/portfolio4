4f013da4cd0aba94aa3b0f89c60daeaf341cf6ce
genix <intergenixyt@gmail.com>
2020-03-15 23:26:10 +0100

    Add home outer buttons (without actions)

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -4,6 +4,12 @@
             łukASZ<br/>
             NieDZiAłek
         </h1>
+        <div id="icons-container">
+            <div id="github-icon" class="outer-button-icon"></div>
+            <div id="yt-icon" class="outer-button-icon"></div>
+            <div id="rive-icon" class="outer-button-icon"></div>
+            <div id="gg-icon" class="outer-button-icon"></div>
+        </div>
     </div>
 </template>
 
@@ -19,15 +25,47 @@
         height: 108pt;
 
         display: flex;
-        align-content: center;
-        justify-content: center;
+        flex-direction: column;
+        justify-content: space-evenly;
+        background-color: #ffc107;
     }
 
     #name-header {
-        color: white;
+        color: black;
         letter-spacing: 4pt;
         padding-left: 4pt; /* to compensate for letter spacing */
         font-family: kontanter, sans-serif;
-        margin: auto; /* this will center it inside its parent */
+    }
+
+    #icons-container {
+        height: 32pt;
+        display: grid;
+        grid-template-columns: repeat(4, 32pt);
+        grid-gap: 8pt;
+        justify-content: center;
+    }
+
+    .outer-button-icon {
+        width: 32pt;
+        height: 32pt;
+        background-position: center;
+        background-size: contain;
+        background-repeat: no-repeat;
+    }
+
+    #yt-icon {
+        background-image: url("../../assets/home/yt_icon.svg");
+    }
+
+    #github-icon {
+        background-image: url("../../assets/home/github_icon.svg");
+    }
+
+    #gg-icon {
+        background-image: url("../../assets/home/gg_icon.svg");
+    }
+
+    #rive-icon {
+        background-image: url("../../assets/home/rive_icon.svg");
     }
 </style>
