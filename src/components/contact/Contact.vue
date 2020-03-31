5b98dbbce92da64ccd7bf07471e5789dcac20730
genix <intergenixyt@gmail.com>
2020-03-31 17:30:11 +0200

    add the rest of contact page

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -1,7 +1,17 @@
 <template>
-    <div style="height: 100vh; display: flex; flex-direction: column;">
+    <div style="height: 100vh; min-height: 360pt; display: flex; flex-direction: column;">
         <NavBar :enlarged-position="4" ref="navBar"/>
         <div id="contact-container">
+            <div style="
+            height:100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+">
+                <div id="contact-me-text">CONTACT ME</div>
+                <div class="contact-info-text">If you have any questions or offers, please feel free to write.</div>
+                <dic id="email-text">lhniedzialek@gmail.com</dic>
+            </div>
         </div>
     </div>
 </template>
@@ -24,8 +34,42 @@
         height: 100%;
         align-self: stretch;
 
-        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../../assets/contact/mailbox.jpg");
+        background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)), url("../../assets/contact/mailbox.jpg");
         background-size: cover;
         background-position: center;
     }
+
+    #contact-me-text {
+        color: white;
+        font-family: cp-mono, serif;
+        font-size: 32pt;
+        font-weight: bold;
+        letter-spacing: 4pt;
+        margin-right: -4pt;
+    }
+
+    .contact-info-text {
+        max-width: 280pt;
+        align-self: center;
+        padding-top: 12pt;
+        padding-bottom: 24pt;
+        text-align: center;
+        color: white;
+        font-family: prime, serif;
+        font-size: 16pt;
+        font-weight: bold;
+    }
+
+    #email-text {
+        align-self: center;
+        background-color: #ffc107;
+        padding: 12pt 16pt;
+        font-family: prime, serif;
+        font-size: 16pt;
+        font-weight: bold;
+        letter-spacing: 2pt;
+        margin-left: -2pt;
+    }
+
+
 </style>
